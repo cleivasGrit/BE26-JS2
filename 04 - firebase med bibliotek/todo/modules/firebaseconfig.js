c46f968344617e505d0ca04c5bd992ee54527057
cleivasGrit <clara.leivas@gritacademy.se>
2026-09-08 15:18:18 +0200

    removed firebase config object

--- 04 - firebase med bibliotek/todo/modules/firebaseconfig.js
+++ 04 - firebase med bibliotek/todo/modules/firebaseconfig.js
@@ -4,13 +4,7 @@ import { getDatabase} from "https://www.gstatic.com/firebasejs/12.18.0/firebase-
   // Your web app's Firebase configuration
   const firebaseConfig = {
 
-    apiKey: "AIzaSyDMyGEyR_r-b3-G68OtkGXXjMIQIuq4cpc",
-    authDomain: "be26-demo.firebaseapp.com",
-    databaseURL: "https://be26-demo-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "be26-demo",
-    storageBucket: "be26-demo.firebasestorage.app",
-    messagingSenderId: "492979691308",
-    appId: "1:492979691308:web:39ae6afd759ec1e1075689"
+    //kopiera infon för din databas från firebase
   };
 
 
